2feec4244a301e50435164cb45cadc0ca6ddbeea
AJoelCastro <misthorn33@gmail.com>
2025-08-22 13:44:28 -0500

    contacto

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,11 +1,13 @@
 
 
 import React from 'react';
-import { Box, Typography, Grid } from '@mui/material';
+
+import { Box, Typography, Grid, Paper } from '@mui/material';
 import { FadeInUp } from '@/components/animations';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
-import Image from 'next/image';
+import SectionContacto from '../../components/SectionContacto';
+import UbicacionMapa from '../../components/UbicacionMapa';
 
 
 export default function Page() {
@@ -13,18 +15,10 @@ export default function Page() {
     <>
       <Navbar />
       <Box sx={{ mt: { xs: 12, md: 14 }, mb: 6 }}>
-        <FadeInUp>
-          <Typography variant="h2" fontWeight={700} gutterBottom color="primary.main" align="center">
-            Contáctanos
-          </Typography>
-        </FadeInUp>
         <FadeInUp delay={0.2}>
-          <Grid container spacing={4} justifyContent="center">
+          <Grid container spacing={6} justifyContent="center" alignItems="flex-start">
             <Grid item xs={12} md={6}>
-              <Image src="/img/contacto1.jpg" alt="Contacto CIDSIA" width={500} height={350} style={{ borderRadius: 16, width: '100%', height: 'auto', objectFit: 'cover' }} />
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Image src="/img/contacto2.jpg" alt="Equipo en contacto" width={500} height={350} style={{ borderRadius: 16, width: '100%', height: 'auto', objectFit: 'cover' }} />
+                <SectionContacto />
             </Grid>
           </Grid>
         </FadeInUp>
